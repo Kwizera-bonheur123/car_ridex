9e3c6b0755c5e4d58b0c2e6035aa74ae9c171094
KWIZERA Bonheur <kbonheur123@gmail.com>
2023-05-21 11:53:51 +0200

    cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,11 +24,27 @@ const Home = (props) => {
                 {props.children}
                 <div>
                     <h1 className='text-3xl my-[30px]'>Get started with 4 simple steps</h1>
-                    <div>
-                        <div className='h-[250px] rounded-xl pt-1 bg-white w-[350px]'>
-                            <div className='rounded-lg px-3 bg-red-100 mx-[42%] my-2 py-3 w-fit'><FaUserPlus className='text-red-800 text-4xl' /></div>
-                            
-                            <h1 className='text-center'>Create a profile<br></br><br></br>If you are going to use a<br></br> passage of Lorem Ipsum, you<br></br> need to be sure.</h1>
+                    <div className='flex gap-8'>
+                        <div className='h-[250px] rounded-xl pt-1 bg-white w-[310px]'>
+                            <div className='rounded-lg px-3 bg-rose-100 mx-[40%] my-2 py-3 w-fit'><FaUserPlus className='text-rose-300 text-2xl' /></div>
+                            <h1 className='text-center font-semibold text-gray-500 text-lg mb-3'>Create a profile</h1>
+                            <h1 className='text-center text-gray-500'>If you are going to use a<br></br> passage of Lorem Ipsum, you<br></br> need to be sure.</h1>
+                            <h1 className='text-blue-600 text-center my-2 font-semibold text-lg'>Get Started</h1>
+                        </div>
+                        <div className='h-[250px] rounded-xl pt-1 bg-white w-[310px]'>
+                            <div className='rounded-lg px-3 bg-blue-100 mx-[40%] my-2 py-3 w-fit'><FaUserPlus className='text-blue-300 text-2xl' /></div>
+                            <h1 className='text-center font-semibold text-gray-500 text-lg mb-3'>Tell us what car you want</h1>
+                            <h1 className='text-center text-gray-500'>Various versions have evolved<br></br> over the years, sometimes by<br></br> accident, sometimes on<br></br>purpose</h1>
+                        </div>
+                        <div className='h-[250px] rounded-xl pt-1 bg-white w-[310px]'>
+                            <div className='rounded-lg px-3 bg-green-100 mx-[40%] my-2 py-3 w-fit'><FaUserPlus className='text-green-300 text22xl' /></div>
+                            <h1 className='text-center font-semibold text-gray-500 text-lg mb-3'>Match with seller</h1>
+                            <h1 className='text-center text-gray-500'>It to make a type specimen<br></br>book. It has survived not only<br></br>five centuries, but also the<br></br>leap into electronic</h1>
+                        </div>
+                        <div className='h-[250px] rounded-xl pt-1 bg-white w-[310px]'>
+                            <div className='rounded-lg px-3 bg-violet-100 mx-[40%] my-2 py-3 w-fit'><FaUserPlus className='text-violet-300 tex2-4xl' /></div>
+                            <h1 className='text-center font-semibold text-gray-500 text-lg mb-3'>Make a deal</h1>
+                            <h1 className='text-center text-gray-500'>There are many variations of<br></br>passages of Lorem available,<br></br>but the majority have suffered<br></br>alteration</h1>
                         </div>
                     </div>
                 </div>
